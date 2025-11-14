bb6b693ada91e7e06a4154b9b0b7a4c737918fcf
Lê Minh Thìn <minhthinle69@gmail.com>
2025-11-14 16:49:41 +0700

    Made use of said API

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod differential_evolution;
 
+use differential_evolution::*;
+
 fn main() {
-    println!("Hello, world!");
+    let config = DEConfig::new()
+        .set_population(200)
+        .set_differential_weight(0.85)
+        .set_crossover_compatibility(0.85);
+    let differential_evolution = DifferentialEvolution::new(config);
 }

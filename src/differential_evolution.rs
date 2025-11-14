e54542a3446cf330f9b64b178de4331a4ff38e0a
Lê Minh Thìn <minhthinle69@gmail.com>
2025-11-14 16:47:59 +0700

    Implemented struct initialisation

--- src/differential_evolution.rs
+++ src/differential_evolution.rs
@@ -1,7 +1,10 @@
+use rand::prelude::*;
+
 pub struct DEConfig {
-    population_size: u32,
+    population_size: usize,
     crossover_compatibility: f32,
     differential_weight: f32,
+    seed: u64,
 }
 
 struct Agent {
@@ -11,31 +14,69 @@ struct Agent {
 pub struct DifferentialEvolution {
     agents: Vec<Agent>,
     config: DEConfig,
+    randomizer: SmallRng,
+}
+
+impl Agent {
+    fn with_data(data: &[f32]) -> Self {
+        Agent {
+            data: data.to_vec(),
+        }
+    }
 }
 
 impl Default for DEConfig {
     fn default() -> Self {
-        Self { population_size: 100, crossover_compatibility: 0.9, differential_weight: 0.8 }
+        Self {
+            population_size: 100,
+            crossover_compatibility: 0.9,
+            differential_weight: 0.8,
+            seed: 0
+        }
     }
 }
 
 impl DEConfig {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self::default()
     }
 
-    fn set_population(mut self, population_size: u32) -> Self {
+    pub fn set_population(mut self, population_size: usize) -> Self {
         self.population_size = population_size;
         self
     }
 
-    fn set_crossover_compatibility(mut self, crossover_compatibility: f32) -> Self {
+    pub fn set_crossover_compatibility(mut self, crossover_compatibility: f32) -> Self {
         self.crossover_compatibility = crossover_compatibility;
         self
     }
 
-    fn set_differential_weight(mut self, differential_weight: f32) -> Self {
+    pub fn set_differential_weight(mut self, differential_weight: f32) -> Self {
         self.differential_weight = differential_weight;
         self
     }
+
+    pub fn set_seed(mut self, seed: u64) -> Self {
+        self.seed = seed;
+        self
+    }
+}
+
+impl DifferentialEvolution {
+    pub fn new(config: DEConfig) -> Self {
+        let mut rng = SmallRng::seed_from_u64(config.seed);
+        let mut agents = Vec::with_capacity(config.population_size);
+
+        for _ in 0..config.population_size {
+            let agent = Agent::with_data(&vec![rng.random::<f32>(), rng.random::<f32>()]);
+            agents.push(agent);
+        }
+
+        Self {
+            config: config,
+            agents,
+            randomizer: rng
+        }
+    }
+
 }

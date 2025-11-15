66352f6d8f9f70c51d03f4093a65ccaa1b40a1ed
Lê Minh Thìn <minhthinle69@gmail.com>
2025-11-15 09:00:46 +0700

    Some minor tweakings

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ use differential_evolution::*;
 fn main() {
     let config = DEConfig::new()
         .set_population(250)
-        .set_differential_weight(0.8)
-        .set_crossover_compatibility(0.9)
-        .set_seed(1234);
+        .set_differential_weight(0.5)
+        .set_crossover_compatibility(0.75)
+        .set_seed(1235);
     let mut differential_evolution = DifferentialEvolution::new(config);
 
+    println!("Before: {:?}", differential_evolution.get_fittest_candidate().unwrap());
     for _ in 0..1000 {
         differential_evolution.step();
     }
-    println!("{:?}", differential_evolution.get_fittest_candidate());
+    println!("After: {:?}", differential_evolution.get_fittest_candidate().unwrap());
 }

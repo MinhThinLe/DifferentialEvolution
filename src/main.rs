6e453f67297be0d7bf30799de11115e1657062b6
Lê Minh Thìn <minhthinle69@gmail.com>
2025-11-26 17:41:53 +0700

    Removed data.txt

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let config = DEConfig::new()
         .set_population(250)
         .set_differential_weight(0.8)
-        .set_crossover_compatibility(0.9)
+        .set_crossover_compatibility(0.1)
         .set_dimensions(3)
         .set_seed(1235);
     let mut differential_evolution = DifferentialEvolution::new(config);
     
-    for _generation in 0..500 {
+    for _generation in 0..50 {
         println!("{}", differential_evolution.get_fittest_candidate().unwrap());
         differential_evolution.step();
     }

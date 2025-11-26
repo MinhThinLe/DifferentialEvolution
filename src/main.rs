a4ea5993fe77842d66cfeb4bf3c525d00c1fd066
Lê Minh Thìn <minhthinle69@gmail.com>
2025-11-26 18:26:57 +0700

    Implemented the best-1 variant of DE

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ fn main() {
     let config = DEConfig::new()
         .set_population(250)
         .set_differential_weight(0.8)
-        .set_crossover_compatibility(0.1)
+        .set_crossover_compatibility(0.9)
         .set_dimensions(3)
         .set_seed(1235);
     let mut differential_evolution = DifferentialEvolution::new(config);
     
     for _generation in 0..50 {
-        println!("{}", differential_evolution.get_fittest_candidate().unwrap());
+        println!("{}", differential_evolution.get_fittest_candidate().unwrap().fitness());
         differential_evolution.step();
     }
 }

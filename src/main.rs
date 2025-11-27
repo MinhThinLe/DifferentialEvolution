098c656b6ae2aaae9d5e2d9dc827430e85e02431
Lê Minh Thìn <minhthinle69@gmail.com>
2025-11-27 10:18:35 +0700

    Feat: benchmarking

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,47 @@
 mod differential_evolution;
 
 use differential_evolution::*;
+use rand::Rng;
+use std::env;
+
+const RUNS: usize = 500;
+const GENERATIONS: usize = 100;
 
 fn main() {
-    let config = DEConfig::new()
-        .set_population(250)
-        .set_differential_weight(0.8)
-        .set_crossover_compatibility(0.9)
-        .set_dimensions(3)
-        .set_seed(1235);
-    let mut differential_evolution = DifferentialEvolution::new(config);
-    
-    for _generation in 0..50 {
-        println!("{}", differential_evolution.get_fittest_candidate().unwrap().fitness());
-        differential_evolution.step();
+    let arguments: Vec<String> = env::args().collect();
+    if arguments.len() < 4 {
+        println!("Insufficient arguments");
+        println!("Usage: <cmd> <population> <weight> <cross-compat>");
+        return;
+    }
+    let population = arguments[1].parse().expect("Invalid population");
+    let differential_weight = arguments[2].parse().expect("Invalid weight");
+    let cross_compatibility = arguments[3].parse().expect("Invalid compatibility");
+
+    let mut data: Vec<Vec<f32>> = Vec::with_capacity(RUNS);
+    let mut rng = rand::rng();
+    for _run in 0..RUNS {
+        let mut current: Vec<f32> = Vec::with_capacity(GENERATIONS);
+        let config = DEConfig::new()
+            .set_population(population)
+            .set_differential_weight(differential_weight)
+            .set_crossover_compatibility(cross_compatibility)
+            .set_dimensions(3)
+            .set_seed(rng.random());
+        let mut differential_evolution = DifferentialEvolution::new(config);
+        for _generation in 0..GENERATIONS {
+            let best_fitness = differential_evolution.get_fittest_candidate().unwrap().fitness();
+            current.push(best_fitness);
+            differential_evolution.step();
+        }
+        data.push(current);
+    }
+    for generation in 0..GENERATIONS {
+        let mut generation_average = 0.0;
+        for run in 0..RUNS {
+            generation_average += data[run][generation];
+        }
+        generation_average /= RUNS as f32;
+        println!("{generation_average}")
     }
 }
